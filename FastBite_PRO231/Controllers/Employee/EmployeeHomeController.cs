03e81873d3df4899a5539369a8922016701dd119
KaKa1234209 <nguyenngocbaochau461@gmail.com>
2026-07-29 18:03:04 +0700

    code

--- FastBite_PRO231/Controllers/Employee/EmployeeHomeController.cs
+++ FastBite_PRO231/Controllers/Employee/EmployeeHomeController.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using FastBite_PRO231.Models;
-using FastBite_PRO231.ViewModels;
+using FastBite_PRO231.ViewModels.Employee;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
--- FastBite_PRO231/obj/Debug/net10.0/FastBite_PRO231.AssemblyInfo.cs
+++ FastBite_PRO231/obj/Debug/net10.0/FastBite_PRO231.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastBite_PRO231")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a75265e25a14ad71b19d90a8c7fe5a900332c636")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e13d6ed15864274594c6a32e056ab9126281a38d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastBite_PRO231")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastBite_PRO231")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
